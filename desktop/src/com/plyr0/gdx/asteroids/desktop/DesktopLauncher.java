a6b839e0d9ed6bef34574a3fcd6f4c6f39bcc619
Mateusz Teklak <mt667@wp.pl>
2016-07-09 21:23:38 +0200

    Initial commit

---
+++ desktop/src/com/plyr0/gdx/asteroids/desktop/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package com.plyr0.gdx.asteroids.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.plyr0.gdx.asteroids.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Game";
+        config.width = 500;
+        config.height = 400;
+        config.resizable = false;
+        config.useGL30 = false;
+		new LwjglApplication(new Game(), config);
+	}
+}
